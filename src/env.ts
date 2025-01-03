157b9245df107da27db1d56d7fb537aaf2a74689
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-01-03 19:43:39 +0900

    T3 Env を設定

--- src/env.ts
+++ src/env.ts
@@ -3,13 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    /**
-     * T3 Env テスト用のURLです
-     */
     DEBUG_URL: z.string().url(),
-    /**
-     * 環境
-     */
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
